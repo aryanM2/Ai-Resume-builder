5e0e9b9bce32f14be7ece3aa43bea314cd61d048
aryan malwa <aryanmalwa07@gmail.com>
2026-06-27 17:01:05 +0530

    Oauthfix

--- server/config/googleAuth.js
+++ server/config/googleAuth.js
@@ -7,7 +7,7 @@ export const configureGoogleAuth = () => {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3000/api/users/google/callback'
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || 'https://airesumebuilder-theta-ten.vercel.app/api/users/google/callback'
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
